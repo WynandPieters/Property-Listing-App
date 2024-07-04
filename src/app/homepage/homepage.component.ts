3a225c71d38c77a4a8a4f5fc5e1a12a2c40fb2ac
Wynand <wynandp@lemonkode.com>
2024-07-04 08:28:29 +0200

    Created email-form component. Added our story text to the homepage with effects.

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { ImageSliderComponent } from './image-slider/image-slider.component';
 import { NavigationButtonsComponent } from './navigation-buttons/navigation-buttons.component';
+import { EmailFormComponent } from "./email-form/email-form.component";
 
 @Component({
-  selector: 'app-homepage-component',
-  standalone: true,
-  imports: [ImageSliderComponent, NavigationButtonsComponent],
-  templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.css'
+    selector: 'app-homepage-component',
+    standalone: true,
+    templateUrl: './homepage.component.html',
+    styleUrl: './homepage.component.css',
+    imports: [ImageSliderComponent, NavigationButtonsComponent, EmailFormComponent]
 })
 export class HomepageComponent {
 
