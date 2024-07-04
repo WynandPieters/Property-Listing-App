89e2ee7c0eedac617e0fcdc0126132679286d1b3
Wynand <wynandp@lemonkode.com>
2024-07-04 22:50:52 +0200

    Created property types model and interface. Started with the price assessment form.

--- src/app/homepage/email-form/email-form.component.ts
+++ src/app/homepage/email-form/email-form.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { PropertyTypes, propertyTypes } from './propertyTypes.model';
 
 @Component({
   selector: 'app-email-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './email-form.component.html',
   styleUrl: './email-form.component.css'
 })
 export class EmailFormComponent {
+  propertyTypes: PropertyTypes[] = propertyTypes;
+
+  priceAssessmentForm: FormGroup = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    emailAddress: ['', [Validators.required, Validators.email]],
+    phoneNumber: ['', Validators.required],
+    valuationDate: ['', Validators.required],
+    propertyType: ['', Validators.required],
+    propertyLocation: ['', Validators.required],
+    solar: ['']
+});
+
+constructor(
+  private formBuilder: FormBuilder,
+  ) {}
+
+onSubmit() {
+  console.log('FORM VALUES', this.priceAssessmentForm)
+}
 
 }
---
+++ src/app/homepage/email-form/propertyTypes.model.ts
@@ -0,0 +1,15 @@
+export interface PropertyTypes {
+    type: string,
+    id: string,
+};
+
+export const propertyTypes: PropertyTypes[] = [
+    { type: 'Apartment', id: 'apa' },
+    { type: 'Farm', id: 'far' },
+    { type: 'General', id: 'gen' },
+    { type: 'Hospitality', id: 'hos' },
+    { type: 'House', id: 'hou' },
+    { type: 'Offices', id: 'off' },
+    { type: 'Retail', id: 'ret' },
+    { type: 'Townhouse', id: 'tow' },
+];
